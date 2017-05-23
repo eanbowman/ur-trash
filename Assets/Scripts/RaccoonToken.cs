44b29e6dfc3e8fad21cc13f9e52f2e4cf255fe2b
Ean Bowman <eanbowman@gmail.com>
2017-05-23 12:01:40 -0400

    Added more to the state machine. For some reason I can't trigger animation states. Putting this down for now.

--- Assets/Scripts/RaccoonToken.cs
+++ Assets/Scripts/RaccoonToken.cs
@@ -12,19 +12,24 @@ public class RaccoonToken : MonoBehaviour {
 	void Awake () {
         this.ai = GetComponent<AICharacterControl>();
         this.animator = GetComponent<Animator>();
+        animator.Play("Walking");
     }
 	
 	// Update is called once per frame
 	void Update () {
         float dist = Vector3.Distance(target.position, transform.position);
-        print("Distance to other: " + dist);
+        //Debug.Log("Distance to other: " + dist);
+        if( dist < 1 )
+        {
+            this.animator.SetBool("Walking", false);
+        }
     }
 
     public void MoveTo(Transform destination) {
         this.target = destination;
         Debug.Log(destination);
         this.ai.SetTarget(destination);
-        this.animator.SetTrigger("Walking");
-        Debug.Log(GetComponent<Animator>());
+        this.animator.SetBool("Walking", true);
+        Debug.Log("Walking state: " + this.animator.GetBool("Walking"));
     }
 }
