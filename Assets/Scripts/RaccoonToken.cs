e23af3586cac79a78ce24257751532014d8df8a0
Ean Bowman <eanbowman@gmail.com>
2017-05-25 21:14:26 -0400

    More work on animation and navigation of tokens

--- Assets/Scripts/RaccoonToken.cs
+++ Assets/Scripts/RaccoonToken.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class RaccoonToken : MonoBehaviour {
-    private AICharacterControl ai;
     private Animator animator;
     private Transform target;
+    private NavMeshAgent nav;
 
 	// Use this for initialization
 	void Awake () {
-        this.ai = GetComponent<AICharacterControl>();
-        this.animator = GetComponent<Animator>();
+        SetReferences();
     }
 	
 	// Update is called once per frame
@@ -19,21 +19,29 @@ public class RaccoonToken : MonoBehaviour {
         CheckDistanceFromDestination();
     }
 
+    void SetReferences() {
+        this.animator = GetComponent<Animator>();
+        this.nav = GetComponent<NavMeshAgent>();
+        this.animator.Play("Raccoon");
+    }
+
     public void MoveTo(Transform destination) {
+        SetReferences();
         this.target = destination;
+        this.nav.SetDestination(this.target.position);
         Debug.Log(destination);
-        this.ai.SetTarget(destination);
         this.animator.SetBool("Walking", true);
         Debug.Log("Walking state: " + this.animator.GetBool("Walking"));
     }
 
     void CheckDistanceFromDestination()
     {
-        float dist = Vector3.Distance(target.position, transform.position);
-        if (this.animator.GetBool("Walking"))
+        float dist = Vector3.Distance(this.target.position, transform.position);
+        if (this.animator.GetBool("Walking") == true)
         {
             if (dist < 1.85f)
             {
+                Debug.Log("Walking state: " + this.animator.GetBool("Walking"));
                 this.animator.SetBool("Walking", false);
                 Debug.Log("Walking state: " + this.animator.GetBool("Walking"));
             }
