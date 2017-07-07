56a2a3e6af3032b1688710bc701136e4e36b54ee
Ean Bowman <eanbowman@gmail.com>
2017-07-07 19:43:26 -0400

    Main Menu Coded! It now launches the new game scene properly, and a scene for credits. Quit quits the editor or the game respectively.

---
+++ Assets/Scripts/UI/HoverInteraction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class HoverInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	public Color colour;
+	public Color hover = Color.red;
+	public Color click = Color.blue;
+	public Text theText;
+
+	void Start() {
+		colour = theText.color;
+		gameObject.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+	}
+
+	public void TaskOnClick() {
+		theText.color = click;
+		Debug.Log (gameObject.name + "Clicked!");
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		theText.color = hover;
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		theText.color = colour;
+	}
+}
