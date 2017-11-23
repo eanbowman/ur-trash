58131386395d5ea81bcaf5e46564e455d2c3f96a
Lilithe Lotor <lilithelotor@gmail.com>
2017-11-22 20:54:39 -0500

    [Game Logic] Deleted everything. Starting over in a branch to see if I can't get the basic game working.

---
+++ raccoon_run/Assets/PathwayHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathwayHandler : MonoBehaviour {
+	public List<Transform> stops;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
