c61a9484fd525d450c3f25cd7b4bc1873956db1d
Lilithe Lotor <lilithelotor@gmail.com>
2018-05-14 21:23:39 -0400

    [Movement][WIP] Started working on the 1-click-to-move code. Rewrote the logic for determining if a move can be made. Currently buggy.

--- raccoon_run/Assets/Scripts/TokenHandler.cs
+++ raccoon_run/Assets/Scripts/TokenHandler.cs
@@ -13,8 +13,8 @@ public class TokenHandler : MonoBehaviour {
 	public float stoppingDistance = 0.5f;
 	public int[] safeSpaces;
 
-	public int destPoint = 0; // the current destination of the token
-	private int nextStep = 0; // the next step toward destPoint
+	public int targetBoardSpace = 0; // the current destination of the token
+	private int nextStep = 0; // the next step toward targetBoardSpace
 	private bool hasStarted = false;
 	private GameController gameController;
 	private bool isMoving = false;
@@ -53,10 +53,10 @@ public class TokenHandler : MonoBehaviour {
 		if (!hasStarted) {
 			if (isSelected) {
 				navMeshAgent.isStopped = false;
-				destPoint = 1; // player is selected, place them at the start
+				targetBoardSpace = 1; // player is selected, place them at the start
 				hasStarted = true;
 			} else {
-				destPoint = 0; // player is not selected, place them in the waiting area
+				targetBoardSpace = 0; // player is not selected, place them in the waiting area
 			}
 		}
 
@@ -99,48 +99,71 @@ public class TokenHandler : MonoBehaviour {
 	}
 
 	void ActivateClickableObject(Vector3 point) {
-		int target = GetClosestObjectID(pathSteps.ToArray(), point);
-		gameController.AddStatus("User clicked close to " + target);
-		//navMeshAgent.SetDestination(target.transform.position);
-		int difference = target - destPoint;
-		if (difference == gameController.diceValue) {
-			isMoving = true;
-			// Find all token objects
-			GameObject[] tokenObjects = GameObject.FindGameObjectsWithTag("Token");
-			// Check if another piece occupies that space
-			GameObject otherObject = GetClosestGameObject(tokenObjects, point, 0.6f);
+		// New strategy: player can only click on their own pieces!
+		// If a piece can move to the space represented by the
+		// number of spaces ahead on the die, then the new destination
+		// is set and the player's token starts moving.
 
-			if (otherObject != null || !pathwayHandler.SetVacancy(target)) {
-				gameController.AddStatus("Clicked on a token!");
-				// If the other object is the player's own token, don't allow the move
-				if (otherObject.GetComponent<TokenHandler>().playerNumber == playerNumber) {
-					gameController.AddStatus("The other token is your own. You can't move there!");
-				} else {
-					gameController.AddStatus("The other token is the opposite player's!");
-					// Check if the other player's piece is on a safe space
-					if (otherObject.GetComponent<TokenHandler>().IsOnSafeSpace()) {
-						gameController.AddStatus("Opponent on safe space");
+		int diceRoll = gameController.diceValue;
+		int nextBoardSpace = targetBoardSpace + diceRoll;
+
+		// Exclude the winner's circle, and check that the nextBoardSpace
+		// is inside of the movable spaces on the board. Player can not
+		// move a token a partial roll ahead.
+		if (nextBoardSpace > pathSteps.Count - 1) {
+			gameController.AddStatus("Token does not have that many spaces left to move. Can not move a partial roll ahead.");
+			return;
+		}
+
+		// Find all token objects
+		GameObject[] tokenObjects = GameObject.FindGameObjectsWithTag("Token");
+		// Check if another piece occupies that space
+		GameObject clickedObject = GetClosestGameObject(tokenObjects, point, 0.6f);
+		// Check if the space diceRoll spaces ahead contains a Token
+		GameObject occupantObject = GetClosestGameObject(tokenObjects, pathSteps[nextBoardSpace].transform.position, 0.6f);
+
+		if(clickedObject && clickedObject.GetComponent<TokenHandler>().playerNumber == this.playerNumber) {
+			// This is our token, we can click it
+			// Check the space diceRoll number of spaces ahead
+			// if it is vacant, move there. Else, show a message.
+			if(pathwayHandler.GetVacancy(nextBoardSpace)) {
+				// We can move there, so move there!
+				pathwayHandler.LeaveSpot(nextStep);
+				targetBoardSpace = nextBoardSpace;
+				pathwayHandler.LeaveSpot(nextStep);
+				pathwayHandler.SetVacancy(targetBoardSpace);
+				gameController.AddStatus("Player " + playerNumber + " moved ahead " + diceRoll + " spaces.");
+			} else {
+				// If the spot isn't vacant, we might still be able to
+				// knock the other token off, if it's the other player's
+				// and it's not on a safe space.
+				if (occupantObject && occupantObject.GetComponent<TokenHandler>().playerNumber == playerNumber) {
+					// We can not move there! Show a message.
+					gameController.AddStatus("Player " + playerNumber + "'s move is blocked by their own token.");
+				} else if (occupantObject) {
+					if (occupantObject.GetComponent<TokenHandler>().IsOnSafeSpace()) {
+						// We can not move there! Show a message.
+						gameController.AddStatus("Player " + playerNumber + "'s move is blocked by an opponent's safe token.");
 					} else {
+						// Opponent is not on a safe space, move there!
 						gameController.AddStatus("Opponent is not safe! They are knocked back to the start.");
-						otherObject.GetComponent<TokenHandler>().KnockBack();
+						clickedObject.GetComponent<TokenHandler>().KnockBack();
 						pathwayHandler.LeaveSpot(nextStep);
-						pathwayHandler.SetVacancy(destPoint);
-						destPoint = target; // we are allowed to take the space
+						pathwayHandler.SetVacancy(targetBoardSpace);
+						targetBoardSpace = nextBoardSpace;
 					}
 				}
-			} else {
-				pathwayHandler.LeaveSpot(nextStep);
-				destPoint = target;
+				
 			}
 		} else {
-			gameController.AddStatus("You can't move there!");
+			gameController.AddStatus("That's not your piece, Player " + playerNumber + "!");
 		}
 	}
 
 	bool IsOnSafeSpace() {
 		bool status = false;
 		foreach (int safeSpace in safeSpaces) {
-			if (destPoint == safeSpace) status = true;
+			if (targetBoardSpace == safeSpace) status = true;
 		}
 
 		return status;
@@ -148,7 +171,7 @@ public class TokenHandler : MonoBehaviour {
 
 	// Reset this token to the start
 	public void KnockBack() {
-		destPoint = 0;
+		targetBoardSpace = 0;
 		nextStep = 0;
 		transform.position = pathSteps[nextStep].position;
 		navMeshAgent.isStopped = true;
@@ -203,24 +226,24 @@ public class TokenHandler : MonoBehaviour {
 
 		// Choose the next point in the array as the destination,
 		// cycling to the start if necessary.
-		// destPoint = (destPoint + 1) % pathSteps.Count;
+		// targetBoardSpace = (targetBoardSpace + 1) % pathSteps.Count;
 
 		// If the target is before the current step, 
 		// or it's the end, set it to the winning circle
-		if (!winner && (destPoint < nextStep || nextStep >= pathSteps.Count - 2)) {
+		if (!winner && (targetBoardSpace < nextStep || nextStep >= pathSteps.Count - 2)) {
 			//navMeshAgent.isStopped = true;
 			//transform.position = pathSteps[pathSteps.Count - 1].position;
 			//transform.rotation = pathSteps[pathSteps.Count - 1].rotation;
-			destPoint = pathSteps.Count - 1;
+			targetBoardSpace = pathSteps.Count - 1;
 			isSelected = false;
 			winner = true;
 			GetComponentInParent<PlayerHandler>().IncrementPoints();
 		}
 
 		// If the target is ahead, keep progressing to the next step.
-		if (destPoint > nextStep) {
+		if (targetBoardSpace > nextStep) {
 			nextStep = (nextStep + 1) % pathSteps.Count;
-		} else if (destPoint == nextStep) {
+		} else if (targetBoardSpace == nextStep) {
 			// We've reached our destination
 			if (IsOnSafeSpace() && isMoving) {
 				// and it's still our turn
